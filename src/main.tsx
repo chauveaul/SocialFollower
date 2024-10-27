83d8778ad2f72152ede8cfc342ceec3ecf9e1d55
MeZe <lucaschauveau007@gmail.com>
2024-10-27 07:16:54 -0400

    Added a few necessary shadcn components

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <p className="text-3xl">test</p>
+  </StrictMode>,
+);
