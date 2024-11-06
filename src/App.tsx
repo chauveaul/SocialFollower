e2639d7dc32fb8b54985e9bcba84260f2ff75de4
MeZe <lucaschauveau007@gmail.com>
2024-11-06 12:41:39 -0500

    Added default dark theme globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Client } from "appwrite";
 import {
   Route,
@@ -20,8 +20,8 @@ export default function App({ routes }) {
   const client = new Client();
   client.setProject("671d9734ace647d7440b");
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 }
