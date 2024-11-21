4d2a8b08cd0f0ab9419229d98267c6f687521069
MeZe <lucaschauveau007@gmail.com>
2024-11-21 16:11:51 -0500

    Added basic email-password sign in and sign up This will need to be revised once the form works as intended

---
+++ src/lib/server/auth/service.ts
@@ -0,0 +1,49 @@
+import { Client, Account, ID, AppwriteException } from "appwrite";
+
+const client =
+  process.env.NODE_ENV === "development"
+    ? new Client()
+        .setEndpoint("http://localhost/v1")
+        .setProject("673e5e130010788ad4c9")
+    : new Client().setProject("671d9734ace647d7440b");
+
+export default class AuthService {
+  static async registerUser(email: string, password: string) {
+    const account = new Account(client);
+
+    try {
+      await account.create(ID.unique(), email, password);
+    } catch (error) {
+      const err = error as AppwriteException;
+
+      //If this error is thrown, there already exists a user with this email
+      throw new AppwriteException(err.message);
+    }
+  }
+
+  static async loginUser(email: string, password: string) {
+    const account = new Account(client);
+
+    const promise = account.createEmailPasswordSession(email, password);
+
+    promise.then(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }
+
+  static async isLoggedIn(): Promise<boolean> {
+    const account = new Account(client);
+
+    try {
+      await account.get();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
