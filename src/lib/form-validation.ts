7c48666d188e308c234e9655431860a5f3e93f2d
MeZe <lucaschauveau007@gmail.com>
2024-11-25 22:03:31 -0500

    added zod schema for registration page. Will implement it with the website tomorrow

--- src/lib/form-validation.ts
+++ src/lib/form-validation.ts
@@ -5,5 +5,7 @@ export function getRegistrationSchema() {
     name: z.string().min(2).max(50),
     email: z.string().min(1).email("This isn't a valid email"),
     password: z.string().min(4),
+    repeatPassword: z.string().min(4),
+    city: z.string().min(2),
   });
 }
