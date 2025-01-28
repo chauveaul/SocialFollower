36552d82263d86ca09199b2baa338133fe362991
MeZe <lucaschauveau007@gmail.com>
2025-01-27 22:42:24 -0500

    Started Sidebar customization
    Need to center elements and make them larger
    Make buttons "togglable" to shift between charts, profile, etc

--- src/components/ui/prebuilts/SidebarTemplate.tsx
+++ src/components/ui/prebuilts/SidebarTemplate.tsx
@@ -3,16 +3,70 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import {
+  PieChartOutline,
+  PersonCircleOutline,
+  SettingsOutline,
+} from "react-ionicons";
 
 export function SidebarTemplate() {
   return (
-    <Sidebar>
-      <SidebarHeader />
+    <Sidebar variant="floating" collapsible="icon">
+      <SidebarHeader>
+        <span>Logo</span>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem key="charts">
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <PieChartOutline
+                      color={"#ffffff"}
+                      title="chart"
+                      height="25px"
+                      width="25px"
+                    />
+                    <span>Charts</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem key="profile">
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <PersonCircleOutline
+                      color={"#ffffff"}
+                      title="profile"
+                      height="25px"
+                      width="25px"
+                    />
+                    <span>Profile</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem key="settings">
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <SettingsOutline
+                      color={"#ffffff"}
+                      title="settings"
+                      height="25px"
+                      width="25px"
+                    />
+                    <span>Settings</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
