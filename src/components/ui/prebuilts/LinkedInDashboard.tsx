877b846fce575fbe2f79a090f97123cc5bba1e84
MeZe <lucaschauveau007@gmail.com>
2025-01-30 12:02:02 -0500

    Changed height of cards inside of linked in dashboard component alongside some few margin changes

--- src/components/ui/prebuilts/LinkedInDashboard.tsx
+++ src/components/ui/prebuilts/LinkedInDashboard.tsx
@@ -1,25 +1,24 @@
 import { LinkedinIcon } from "@/assets/Icons";
 import { CardTemplate } from "@/components/ui/prebuilts/CardTemplate";
-import { Card } from "../card";
 
 export function LinkedInDashboard() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="m-4 flex flex-col gap-8">
       {/*NOTE:  ^^^^ This will be the overall container that will define the size of the section */}
       {/* Important for the correct sizing to see the next tab appear at the bottom of the screen */}
       <div className="flex text-white/[0.8] gap-4 items-center">
         <LinkedinIcon />
         <h1 className="text-7xl">LinkedIn</h1>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-4">
         {/* NOTE: This is the div for the top layer of the cards */}
         <div className="flex justify-between gap-2">
-          <CardTemplate className="w-full m-2" cardContent={<p>Test</p>} />
+          <CardTemplate className="w-full m-2 h-72" cardContent={<p>Test</p>} />
           <CardTemplate className="w-full m-2" cardContent={<p>Test</p>} />
         </div>
         {/* NOTE: This is the div for the bottom layer of the cards */}
-        <div className="flex">
-          <CardTemplate className="w-full m-2" cardContent={<p>Test</p>} />
+        <div className="flex justify-between gap-2">
+          <CardTemplate className="w-full m-2 h-52" cardContent={<p>Test</p>} />
           <CardTemplate className="w-full m-2" cardContent={<p>Test</p>} />
           <CardTemplate className="w-full m-2" cardContent={<p>Test</p>} />
         </div>
