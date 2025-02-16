b4e3d6a52e5d952d395f7bd5396fc5b317675d7f
MeZe <lucaschauveau007@gmail.com>
2025-02-16 11:30:04 -0500

    Added instagram auth logic

--- src/lib/server/instagram/service.ts
+++ src/lib/server/instagram/service.ts
@@ -1,19 +1,49 @@
-import { Client } from "appwrite";
-//import {
-//  getDocument,
-//  updateDocument,
-//  createDocument,
-//} from "@/lib/server/database/controller";
-//import { getUserInfo } from "@/lib/server/auth/controller";
+import { Client, Functions, Permission, Role } from "appwrite";
+import {
+  getDocument,
+  updateDocument,
+  createDocument,
+} from "@/lib/server/database/controller";
+
+import { getUserInfo } from "@/lib/server/auth/controller";
 
 export default class InstagramService {
-  static async LoginUser() {
+  static async LoginUser(code: string) {
     const client = new Client();
-    //const functions = new Functions(client);
+    const functions = new Functions(client);
 
     client.setProject("671d9734ace647d7440b");
 
-    //const account = await getUserInfo();
-    //const accountId = account.$id;
+    const account = await getUserInfo();
+    const accountId = account.$id;
+
+    const promise = functions.createExecution("67af921e000e2196277c", code);
+    promise.then(async (res) => {
+      if (res.status === "completed") {
+        getDocument("67a67744001f4587566f", "InstagramAuth", accountId)
+          .then(async () => {
+            await updateDocument(
+              "67a67744001f4587566f",
+              "InstagramAuth",
+              accountId,
+              { accessToken: res.responseBody },
+            );
+          })
+          .catch(async () => {
+            await createDocument(
+              "67a67744001f4587566f",
+              "InstagramAuth",
+              accountId,
+              { accessToken: res.responseBody },
+              [
+                Permission.write(Role.user(accountId)),
+                Permission.read(Role.user(accountId)),
+                Permission.update(Role.user(accountId)),
+              ],
+            );
+          });
+        window.location.href = "https://socialfollower.xyz/Dashboard";
+      }
+    });
   }
 }
